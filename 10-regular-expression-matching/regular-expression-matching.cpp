95a3d07eda18a320ba20f97805269ed1c4d0242d
Suhani180 <147927945+Suhani180@users.noreply.github.com>
2026-08-05 23:39:14 +0530

    Time: 1 ms (64.03%) | Memory: 9.1 MB (57.31%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
 
-    vector<vector<int>> dp;
-
-    bool solve(int i, int j, string &s, string &p){
-        if(j ==p.size()){
-            return i== s.size();
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        bool match = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
-
-        if(j+1 <p.size() && p[j + 1] == '*'){
-            return dp[i][j] = solve(i, j+2,s, p) || (match && solve(i+1, j,s,p));
-        }
-        return dp[i][j] = match && solve(i +1, j+1,s,p);
-    }
 
     bool isMatch(string s, string p) {
-        dp.assign(s.size()+1,vector<int>(p.size() +1,-1));
-        return solve(0,0,s,p);
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+        dp[0][0] = true;
+        for(int j =2; j<=m; j++){
+            if(p[j-1] == '*'){
+                dp[0][j] = dp[0][j-2];
+            }
+        }
+        for(int i =1; i<=n; i++){
+            for(int j =1; j<=m; j++){
+                if(p[j-1] == '.' || p[j-1] == s[i-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1] == '*'){
+                    dp[i][j] = dp[i][j-2];
+                    if(p[j-2] == '.' || p[j-2] == s[i-1]){
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
